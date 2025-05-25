7a9b91253af83fefdfd552e8860bd63e7cbb1b10
Baadshah-Abdul <abdulhaque1732@gmail.com>
2025-05-25 20:35:01 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -92,8 +93,59 @@ while(1)
 		}
 		continue;
 	}
-	printf("%s: not found\n", input);
+
+    //Handle external command
+	char *args[10];
+	int arg_count = 0;
+	char *token = strtok(input, " ");
+
+	//extract arguments
+	while (token != NULL && arg_count < 10)
+		{
+			args[arg_count++] = token;
+			token = strtok(NULL, " ");
+		}
+	args[arg_count] = NULL; //NULL terminate argument list
+		
+	//check for executable in PATH
+	char *exe_path = NULL;
+        char *path = getenv("PATH");
+        if(path != NULL) {
+            char *path_copy = strdup(path);
+            char *direc = strtok(path_copy, ":");
+            
+            while(direc != NULL) {
+                char full_path[512];
+                snprintf(full_path, sizeof(full_path), "%s/%s", direc, args[0]);
+				//check if executable
+                if(access(full_path, X_OK) == 0) {                      exe_path = strdup(full_path);   // Store found path
+                    break;
+                }
+                direc = strtok(NULL, ":");
+            }
+            free(path_copy);	
+			}
+
+			if(exe_path != NULL) 
+			{
+				pid_t pid = fork();  // Create child process
+				if(pid == 0) 
+				{
+                // Child process - execute program
+					execv(exe_path, args);
+					perror("execv");  					exit(1);
+				}
+				else if(pid > 0) {
+					// Parent process - wait for child
+					wait(NULL);
+				}
+				else
+					perror("fork");  
+                free(exe_path); 
+			}
+			else
+				printf("%s: command not found \n", args[0]);
 }
 
-  return 0;
+			return 0;
 }
