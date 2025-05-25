b3fe884f93e72fb8c5baacbfd784d297565e8db9
Baadshah-Abdul <abdulhaque1732@gmail.com>
2025-05-25 19:14:34 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -38,27 +38,60 @@ while(1)
 	//check for type commands
 	if(strcmp(command,"type") == 0)
 	{
-		char *type_command = input + 5;
-		char *space = strchr(type_command, ' ');
-		//NULl terminate the command at the first space
-		if(space != NULL)
-			*space = '\0';
+		//extract command for path
+		char type_command[100] = {};
+		char *command_ptr = input + 5;
+		while(*command_ptr == ' ')
+			command_ptr++;
+		sscanf(command_ptr, "%s", type_command);
 		
-		int found = 0;
+		//check for builtin
+		int found_type = 0;
 		for(int i = 0; i < n; i++)
 		{
 			if(strcmp(type_command, shell_cmd[i]) == 0)
 			{
 				printf("%s is a shell builtin\n", type_command);
-				found = 1;
+				found_type = 1;
 				break;
 			}
 		}
-		if(!found)
-		printf("%s: not found\n", type_command);
+ 
+	//check PATH if not builtin
+		if(!found_type)
+		{
+			char *path = getenv("PATH");
+			if(path != NULL)
+			{
+				char *path_copy = strdup(path);
+				char *direc = strtok(path_copy, ":");
+				int found_path = 0;
+
+				while(direc != NULL)
+				{
+					//construct proper path with separator
+					char full_path[512];
+					snprintf(full_path, sizeof(full_path), "%s/%s", direc, type_command);
+					//check if executable
+					if (access(full_path, X_OK) == 0)
+					{
+						printf("%s is %s\n", type_command, full_path);
+						found_path = 1;
+						break;
+					}
+					direc = strtok(NULL, ":");
+				}
+				//free space from strdup()
+				free(path_copy);
+
+				if(!found_path)
+					printf("%s: not found\n", type_command);
+			}
+			else
+				printf("%s: not found\n", type_command);
+		}
 		continue;
 	}
-
 	printf("%s: not found\n", input);
 }
 
