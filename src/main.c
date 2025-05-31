81a3f424a903ae4fea8d279eee0207883f3f985e
Baadshah-Abdul <abdulhaque1732@gmail.com>
2025-05-31 23:59:36 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -377,6 +377,7 @@ int main(int argc, char *argv[])
 		// check for history
 		if (strcmp(command, "history") == 0)
 		{
+			//from readline/history.h
 			HIST_ENTRY **the_list = history_list();
 			if (the_list)
 			{
